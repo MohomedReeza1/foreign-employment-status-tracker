df0fbf8cede48971efe1fffcc0254c16c100b494
Mohomed Reeza <mohomedreeza011210@gmail.com>
2025-05-27 00:34:27 +0530

    Initialized Alembic + Modified database.py and main.py

---
+++ backend/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.database import engine, Base
+# from app.api import auth
+# from app.api import candidate
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"msg": "Backend API is running"}
+
+# app.include_router(auth.router)
+# app.include_router(candidate.router)
