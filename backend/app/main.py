f686eaaa9f612c14090dc3fd080e3bfd293e618c
Mohomed Reeza <mohomedreeza011210@gmail.com>
2025-05-30 15:00:14 +0530

    Added CORSMiddleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app.api import auth
 from app.api import candidate
@@ -6,6 +7,15 @@ from app.api import candidate
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # 👈 Your frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
