b46809b88e66c910c75f8b33ee59b0acbb82b9a1
Mohomed Reeza <mohomedreeza011210@gmail.com>
2025-05-30 14:58:34 +0530

    Commented out <AuthProvider> for other developments

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import { AuthProvider } from './context/AuthContext'
+// import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <AuthProvider>
+    {/* <AuthProvider> */}
       <App />
-    </AuthProvider>
+    {/* </AuthProvider> */}
     </BrowserRouter>
   </React.StrictMode>,
 )
